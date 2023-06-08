bc4cade43d3c69e1ab29955b45e41f17cdf45f52
Nicole Arwood <nsarwood@gmail.com>
2023-06-08 01:59:34 -0400

    Task 1-5, part of 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,62 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let usr = req.params.username;
+  let pwd = req.params.password;
+  if (!(usr&&pwd)) {
+    return res.status(400).json(usr+pwd);
+  }
+  var unq = true;
+  for (var user in users) {
+      if (user["username"]===usr) {
+          unq = false;
+      }
+  }
+  if (unq) {
+      users.push({"username": usr, "password": pwd})
+      return res.status(300).json({message: "User sucessfully registered."});
+  }
+  else {return res.status(400).json({message: "User already registered."});}
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   let bookList = [];
+   for (var i in books) {
+       bookList.push(books[i]["title"]);
+   }
+   return res.status(300).json(bookList);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = parseInt(req.params.isbn);
+  return res.status(300).json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  for (var i in books) {
+      if (books[i]["author"]===req.params.author) {
+        return res.status(300).json(books[i]);
+      }
+  }
+  return res.status(300).json("No matching author found. Check spelling/capitalization.");
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for (var i in books) {
+        if (books[i]["title"]===req.params.title) {
+          return res.status(300).json(books[i]);
+        }
+    }
+    return res.status(300).json("No matching title found. Check spelling/capitalization.");
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(books[parseInt(req.params.isbn)]["reviews"]);
 });
 
 module.exports.general = public_users;
